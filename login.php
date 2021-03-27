67db1c35dd0a37cd3e8d9c500d3c12cac43cfdde
dewangga-pk <pacetcommunity570@gmail.com>
2021-03-27 12:29:46 +0700

    users controller and dynamic views

--- login.php
+++ login.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chatkuy | Realtime Chat App</title>
-    <link rel="stylesheet" href="asset/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
-</head>
+<?php include_once("components/header.php") ?>
 <body>
     <div class="wrapper">
         <section class="form login">
--- users.php
+++ users.php
@@ -1,23 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Chatkuy | Realtime Chat App</title>
-    <link rel="stylesheet" href="asset/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
-</head>
+<?php
+    session_start();
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+    include_once("controller/config.php");
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+    if(mysqli_num_rows($sql)>0){
+        $row = mysqli_fetch_assoc($sql);
+    }
+?>
+<?php include_once("components/header.php") ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
                 <div class="content">
-                    <img src="https://avatars.githubusercontent.com/u/55013652?v=4" alt="">
+                    <img src="controller/storage/<?= $row['img'] ?>" alt="">
                     <div class="details">
-                        <span>Andromeda</span>
-                        <p>Active now</p>
+                        <span><?= $row['fname'] . " " . $row['lname'] ?></span>
+                        <p><?= $row['status'] ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -28,76 +29,7 @@
                 <button><i class="fas fa-search"></i></button>
             </div>
             <div class="user-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot offline"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="https://avatars.githubusercontent.com/u/45931684?v=4" alt="">
-                        <div class="details">
-                            <span>Iqbal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
+
             </div>
         </section>
     </div>
